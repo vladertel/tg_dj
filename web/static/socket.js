298c24f71750f3692b4c29dc2fcaf54d0a2d7905
Vasiliy N. Polikaepov <privetik13@gmail.com>
2018-09-29 16:18:13 +0300

    Fix issue where web-sockets does not work

--- web/static/socket.js
+++ web/static/socket.js
@@ -1,16 +1,28 @@
-var address = "wss://212.86.109.84:8081/ws";
+(function(){
+    "use strict";
 
-wson = WSON(address)
+    var wson = WSON("ws://212.86.109.84:8081/ws");
+    let last_keep_alive = Date.now();
 
-function on_update(data) {
-    console.log("got data");
-    console.log(data);
-}
+    function on_update(data) {
+        document.getElementById("title").innerText = data.title;
+        document.getElementById("duration").innerText = data.duration;
+    }
 
-function on_keep_alive() {
-    console.log("got keep_alive");
-}
+    function on_keep_alive() {
+        last_keep_alive = Date.now();
+    }
 
 
-wson.on("update", on_update)
-wson.on("keep_alive", on_keep_alive)
+    setTimeout(function(){
+        var time_delta = Date.now() - last_keep_alive;
+        if (time_delta.minute > 3){
+            location.reload();
+        } else {
+            console.log("Alive: OK");
+        }
+    }, 60000);
+
+    wson.on("update", on_update);
+    wson.on("keep_alive", on_keep_alive);
+})();
