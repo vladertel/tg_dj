debcc692518ec24cf3e787e506473e28452322d0
Vladertel <Deadvladking@gmail.com>
2018-09-29 15:19:14 +0300

    Fixes

--- web/static/socket.js
+++ web/static/socket.js
@@ -2,12 +2,14 @@ var address = "wss://212.86.109.84:8081/ws";
 
 wson = WSON(address)
 
-function on_update(data):
-    console.log("got data")
-    console.log(data)
+function on_update(data) {
+    console.log("got data");
+    console.log(data);
+}
 
-function on_keep_alive():
-    console.log("got keep_alive")
+function on_keep_alive() {
+    console.log("got keep_alive");
+}
 
 
 wson.on("update", on_update)
