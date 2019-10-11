6489f7961a3f7637b488f89b583bebc254a10153
Vasiliy N. Polikarpov <vasiliy.polikarpov@pimpay.ru>
2019-10-11 16:55:10 +0300

    Retry websocket connection after failure

--- web/static/wson.js
+++ web/static/wson.js
@@ -5,11 +5,42 @@
  * @constructor
  */
 function WSON(address){
-    var ws = new WebSocket(address);
+    var ws = null;
     var handlers = {};
     var requests = {};
+    var open_handlers = [];
+    var close_handlers = [];
 
-    ws.onmessage = function(event){
+    function init(){
+        console.log("WS connection init...");
+        var connection_timeout = setTimeout(() => ws.close(), 10000);
+        ws = new WebSocket(address);
+        ws.onopen = function () {
+            clearTimeout(connection_timeout);
+            console.log("WS connection established");
+            for (var i = 0; i < open_handlers.length; i++){
+                open_handlers[i]();
+            }
+        };
+        ws.onmessage = on_message;
+        ws.onclose = function () {
+            clearTimeout(connection_timeout);
+            console.log("WS connection closed");
+            for (var i = 0; i < close_handlers.length; i++){
+                close_handlers[i]();
+            }
+            setTimeout(init, 1000);
+        };
+        ws.onerror = function (err) {
+            clearTimeout(connection_timeout);
+            console.log("WS error: ", err);
+            ws.close();
+        };
+    }
+
+    init();
+
+    function on_message(event){
         console.log(event.data);
 
         var data = event.data;
@@ -30,10 +61,10 @@ function WSON(address){
     };
 
     this.onopen = function(handler){
-        ws.onopen = handler;
+        open_handlers.push(handler);
     };
     this.onclose = function(handler){
-        ws.onclose = handler;
+        close_handlers.push(handler);
     };
 
     this.on = function(msg, handler){
